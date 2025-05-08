34babeb2c4f6be7ddb34768e05ad82baf67d1333
Hugo Mamona <113359386+hmma0308@users.noreply.github.com>
2025-05-08 22:25:11 +0000

    database connection

--- api/models/index.js
+++ api/models/index.js
@@ -1,26 +1,46 @@
-import configdb from '../config/db.config.js';
+import dbConfig from '../config/db.config.js';
 import { Sequelize } from 'sequelize';
 import User from './User.js';
-import pg from "pg";
+import pg from 'pg';
 
-const sequelize = new Sequelize(
-    configdb.database,
-    configdb.user,
-    configdb.password,
-    {
-        host:configdb.host,
-        dialect: configdb.dialect,
-        port: configdb.port,
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
         dialectModule: pg,
+        dialectOptions: process.env.DATABASE_URL.includes('neon.tech') ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        } : {},
         pool: {
-            mas: configdb.pool.max,
-            min: configdb.pool.min,
-            acquire: configdb.pool.acquire,
-            idle: configdb.pool.idle,
-            evict: configdb.pool.evict,
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle,
+            evict: dbConfig.pool.evict
         },
-    }
-);
+        logging: console.log
+    })
+    : new Sequelize(
+        dbConfig.database,
+        dbConfig.user,
+        dbConfig.password,
+        {
+            host: dbConfig.host,
+            dialect: dbConfig.dialect,
+            port: dbConfig.port,
+            dialectModule: pg,
+            pool: {
+                max: dbConfig.pool.max,
+                min: dbConfig.pool.min,
+                acquire: dbConfig.pool.acquire,
+                idle: dbConfig.pool.idle,
+                evict: dbConfig.pool.evict
+            },
+            logging: console.log
+        }
+    );
 
 const db = {};
 db.Sequelize = Sequelize;
--- example_requests/Token_Valid.sh
+++ example_requests/Token_Valid.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url 'http://localhost:3000/secureExampleRoute/' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDYzODg1MDQsImV4cCI6MTc0NjM5MjEwNH0.3WL6awmzk_kQYHYpQM_Q4QzaWSmbasvG7hwQE8nmMTw'
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDY0MDAxNTksImV4cCI6MTc0NjQwMzc1OX0.gZsg1dIR22aL_BaTTDOZ5iT7uStIm9ncZ9nclHXX30Y'
